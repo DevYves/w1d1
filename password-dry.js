990ffb99e90414dbe9646804fec3b1dccb84e393
DevYves <yfreypons@gmail.com>
2017-10-31 23:59:15 +0000

    made password obfuscate code DRY

---
+++ password-dry.js
@@ -0,0 +1,31 @@
+function obfuscateLetter(letter){
+ switch (letter){
+    case 'a': {
+      return '4';
+    }
+    case 'e': {
+      return '3';
+    }
+    case 'o': {
+      return '0';
+    }
+    case 'l': {
+      return '1';
+    }
+
+  default: return letter;
+
+ }
+}
+function obfuscate(password){
+  var result = ''
+  for (var i = 0; i < password.length; i++){
+    result += obfuscateLetter(password[i]);
+
+ }
+ return result;
+
+}
+
+console.log(obfuscate(process.argv[2]));
+
